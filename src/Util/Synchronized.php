ebaaa38453ab9a0f62b041852f8e07a2bd3b5df0
Frode Børli <frode@ennerd.com>
2025-12-28 00:28:57 +0000

    fix: Detect reentrant lock attempts in Synchronized
    Throws LogicException instead of silently deadlocking when the same
    context attempts to acquire a lock it already holds. Uses phasync
    context tracking to detect reentry across nested phasync::run() calls.

--- src/Util/Synchronized.php
+++ src/Util/Synchronized.php
@@ -2,38 +2,49 @@
 
 namespace phasync\Util;
 
-use phasync\TimeoutException;
-
 /**
- * Placeholder class for supporting thread safe synchronization. The current
- * implementation is not thread safe, but is safe from within fibers.
+ * Simple coroutine-safe mutex for synchronizing access to shared resources.
+ *
+ * This implementation is not reentrant - attempting to acquire the same lock
+ * from within a locked context will throw an exception. For reentrant locking,
+ * use LockTrait instead.
+ *
+ * Note: Not thread safe if PHP threading is enabled.
  */
 final class Synchronized
 {
     private static array $locks = [];
+    private static array $holders = [];
 
     /**
      * Run a function ensuring that the function will not be invoked by other
      * coroutines at the same time.
      *
-     * @throws TimeoutException
-     * @throws \Throwable
+     * @throws \LogicException if attempting reentrant lock from same context
+     * @throws \Throwable if the closure throws
      */
     public static function run(object|string $token, \Closure $closure): mixed
     {
         if (\is_object($token)) {
             $token = \spl_object_hash($token);
         }
+
+        $currentContext = \phasync::getContext();
+        if (isset(self::$holders[$token]) && self::$holders[$token] === $currentContext) {
+            throw new \LogicException('Synchronized::run() is not reentrant; use LockTrait for reentrant locking');
+        }
+
         while (isset(self::$locks[$token])) {
             \phasync::awaitFlag(self::$locks[$token]);
         }
         try {
             self::$locks[$token] = new \stdClass();
+            self::$holders[$token] = $currentContext;
 
             return $closure();
         } finally {
             \phasync::raiseFlag(self::$locks[$token]);
-            unset(self::$locks[$token]);
+            unset(self::$locks[$token], self::$holders[$token]);
         }
     }
 }
